515a581a911a3a62c053d1381f66080e27e1b34f
azaliasafira <azaliafira16@gmail.com>
2022-04-25 10:02:35 +0700

    konfigurasi templates

--- application/controllers/MDashboard.php
+++ application/controllers/MDashboard.php
@@ -1,12 +1,14 @@
 <?php
 
-class MDashboard extends CI_Controller {
+class MDashboard extends CI_Controller
+{
 
     public function index()
     {
+        // $this->load->view('templates/navbar');
+        $this->load->view('templates/header');
+        $this->load->view('templates/sidebar');
         $this->load->view('marketing/dashboard/index');
+        $this->load->view('templates/footer');
     }
-
 }
-
-?>
---
+++ application/controllers/PDashboard.php
@@ -0,0 +1,10 @@
+<?php
+    class PDashboard extends CI_Controller {
+
+        public function index()
+        {
+            $this->load->view('portir/dashboard/index');
+        }
+    
+    }
+?>
