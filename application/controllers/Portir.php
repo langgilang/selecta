baca4ec034101b8997e9cabe9fe1d89ac5be3aa1
azaliasafira <azaliafira16@gmail.com>
2022-05-17 07:48:34 +0700

    .

--- application/controllers/Portir.php
+++ application/controllers/Portir.php
@@ -1,19 +1,9 @@
 <?php
 class Portir extends CI_Controller
 {
-
     public function index()
     {
-        $this->load->view('portir/templates/header');
-        $this->load->view('portir/templates/sidebar');
-
-        $data = array(
-            'header' => 'Dashboard'
-        );
-
-        $this->load->view('portir/dashboard/index_tampil', $data);
-        $this->load->view('portir/templates/footer');
+        $this->template->load('portir/templates', 'portir/tiketoffline/tiketoffline_tampil');
         // $this->load->helper('url');
-
     }
 }
